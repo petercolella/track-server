936964b7ee1a3f3ef8340bdf2678abb90ece30e9
petercolella <peter.joseph.colella@gmail.com>
2020-06-22 16:34:12 -0400

    179. adds jwt to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req, res) => {
   try {
     const dbModel = await db.User.create(req.body);
-    res.send(dbModel);
+    const token = jwt.sign({ userId: dbModel._id }, 'MY_SECRET_KEY');
+    res.send({ token });
   } catch (err) {
-    res.send(err);
+    return res.status(422).send(err.message);
   }
 });
 
