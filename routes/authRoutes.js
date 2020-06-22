4da32a615692aef13a545bdc3cfbfaf94438c67b
petercolella <peter.joseph.colella@gmail.com>
2020-06-22 15:53:56 -0400

    updates User signup post route to async/await

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-router.post('/signup', (req, res) => {
-  db.User.create(req.body)
-    .then(dbModel => res.send(dbModel))
-    .catch(err => res.send(err));
+router.post('/signup', async (req, res) => {
+  try {
+    const dbModel = await db.User.create(req.body);
+    res.send(dbModel);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 module.exports = router;
